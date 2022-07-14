cfe11421a713e2f80c0d704f1401d071f56695d8
Eirik Berger <eirik.berger@me.com>
2022-07-14 17:41:20 +0200

    script not working. Will change setup

--- R/main.R
+++ R/main.R
@@ -1,72 +1,75 @@
+#' Create enviroment with input information
+#' @export
+hismatch_settings <- function(...){
+    .hismatch_env <<- rlang::env(..., parent = emptyenv())
+}
+
 #' Create variables to link on
 #' @export
-addLinkingVariables <- function(data_input, firstname_input, lastname_input){
-  data_input[, full_name:=tolower(paste(get(..firstname_input), get(..lastname_input)))][
-    , l_first := str_match(tolower(get(..firstname_input)), "(^[:alpha:])")[,1]][
-    !is.na(l_first)][
-    , l_sur := str_match(full_name, " ([:alpha:])[a-zæøå.]*?$")[,2]][
-    !is.na(l_sur)][
-    , masterID := .I]
+addLinkingVariables <- function(data_input){
+  data_input[, full_name:=tolower(paste(get(.hismatch_env$firstname), get(.hismatch_env$surname)))][
+    , l_first := str_match(tolower(get(.hismatch_env$firstname)), "(^[:alpha:])")[,1]][
+    !is.na(l_first)][, l_sur := str_match(full_name, " ([:alpha:])[a-zæøå.]*?$")[,2]][
+    !is.na(l_sur)][, masterID := .I]
 }
 
 #' Names names ('_1' and '_2')
 #' @export
-setNames <- function(data_input, suffix, block=c('')){
+setNames <- function(data_input, suffix){
   for (j in colnames(data_input)){ 
-    if (j %nin% block) {setnames(data_input, c(j), paste0(j, suffix))}
+    if (j %nin% .hismatch_env$blocks) {setnames(data_input, c(j), paste0(j, suffix))}
   }
 }
 
 #' Create parsimonious dataset for linking
 #' @export
-createLinkingData <- function(data_input, block=c('')){
-  keep <- append(unlist(strsplit(block, ", ")), c('full_name', 'masterID'))
+createLinkingData <- function(data_input){
+  keep <- append(unlist(strsplit(.hismatch_env$blocks, ", ")), c('full_name', 'masterID'))
   data_input[,..keep]
 }
 
 #' Create blocks
 #' @export
-createBlocks <- function(data, block=c('')){
-  keep <- unlist(strsplit(block, ", "))
+createBlocks <- function(data){
+  keep <- unlist(strsplit(.hismatch_env$blocks, ", "))
   comb <- data[,..keep][!is.na(l_first) & !is.na(l_sur)]
   unique(comb, by = keep)
 }
 
 #' Setup for linking using other functions
 #' @export
-setupData <- function(data, firstname, surname, block_vector, suffix){
+setupData <- function(data_input, suffix){
   
-  dta_full <- hismatch:::addLinkingVariables(data_input=data, 
-                                   firstname_input=paste0(firstname), 
-                                   lastname_input=paste0(firstname))
-  
-  dta_comp <- hismatch:::createLinkingData(dta_full,
-                                 block=block_vector)
-  
-  hismatch:::setNames(dta_full, suffix=suffix, block=block_vector)
-  hismatch:::setNames(dta_comp, suffix=suffix, block=block_vector)
-  
-  blocks <- hismatch:::createBlocks(dta_comp, 
-                         block=block_vector)
+  dta_full <- hismatch:::addLinkingVariables(data_input=data_input)
+  dta_comp <- hismatch:::createLinkingData(dta_full)  
+  hismatch:::setNames(dta_full, suffix=suffix)
+  hismatch:::setNames(dta_comp, suffix=suffix)
+  blocks <- hismatch:::createBlocks(dta_comp)
   
   return(list(dta_full, dta_comp, blocks))
 }
 
 #' Fuzzy mathing
 #' @export
-fuzzyMatch<- function(data1, data2, block_number, blocks_tmp, dist_thr, rel_thr, blocks){
+fuzzyMatch<- function(data1, data2, block_number, blocks_tmp, blocks){
+    
+  stopifnot(is.data.table(data1) & is.data.table(data2))
 
   for (blocks_variable in blocks) {
-    data1 <- data1[blocks_variable==paste0(blocks_tmp[[c(block_number), blocks_variable]])]
-    data2 <- data2[blocks_variable==paste(blocks_tmp[[c(block_number), blocks_variable]])]
+    ex <- blocks_tmp[[c(block_number), blocks_variable]]
+    data1 <- subset(data1, get(blocks_variable)==paste0(ex))
+    data2 <- subset(data2, get(blocks_variable)==paste0(ex))
   }
-  
+    
   merge_dataset <- merge(data1, data2, by=blocks, allow.cartesian=TRUE)
   
+  stopifnot(is.data.table(merge_dataset))
+
   # string distance and execute matching rules
-  merge_dataset <- merge_dataset[,dist:=stringdist::stringsim(full_name_1, full_name_2, method = c("jw"))][
-    , rank1 := frank(-dist, ties.method='max'), by = "masterID_1"][
-    , rank2 := frank(-dist, ties.method='max'), by = "masterID_2"][
+  merge_dataset <- merge_dataset[, dist:=stringsim(full_name_1, full_name_2, method = c("jw"))]
+  merge_dataset <- merge_dataset[
+    , rank1:=frank(-dist, ties.method='max'), by = "masterID_1"][
+    , rank2:=frank(-dist, ties.method='max'), by = "masterID_2"][
     order(-dist)]
   
   merge_dataset <- merge_dataset[
@@ -75,29 +78,26 @@ fuzzyMatch<- function(data1, data2, block_number, blocks_tmp, dist_thr, rel_thr,
     , rel1:=mean(sum1, na.rm=T)/dist, by="masterID_1"][
     , rel2:=mean(sum2, na.rm=T)/dist, by="masterID_2"]
   
-  merge_dataset[
-    rank1==1 & rank2==1]
+  merge_dataset[rank1==1 & rank2==1]
 }
 
 #' Loop over fuzzy matching using the future package
 #' @export
-executeLinking <- memoise::memoise(function(data1, data2, blocks_data, 
-        dist_thr, rel_thr, blocks) {
-  p <- progressor(steps = nrow(blocks_data))
+executeLinking <- memoise::memoise(function(data1, data2, blocks_data) {
+
+  p <- progressr::progressor(steps = nrow(blocks_data))
   
   future_map_dfr(1:nrow(blocks_data), ~{
     p()
     hismatch:::fuzzyMatch(data1=data1, 
                data2=data2, 
                block_number=.x, 
-               blocks_tmp=blocks_data,
-               dist_thr=dist_thr, 
-               rel_thr=rel_thr, 
-               blocks=blocks)
-  })
+               blocks_tmp=blocks_data, 
+               blocks=.hismatch_env$blocks)
+  }, .options = furrr_options(packages=c("data.table", "stringdist")))
 }, cache=cachem::cache_disk(rappdirs::user_cache_dir("hismatch")))
 
-#' Merge back in full data
+#' Merge back in full datas
 #' @export
 mergeBackInData <- function(merged_data, master1, master2){
   skeleton <- merged_data[,.(masterID_1, masterID_2)]
@@ -108,59 +108,41 @@ mergeBackInData <- function(merged_data, master1, master2){
 
 #' Reorder variables
 #' @export
-vectorReorder <- function(dataset, blocks){
+vectorReorder <- function(dataset){
   vector_reorder <- colnames(dataset)
-  vector_reorder <- append(blocks, vector_reorder[vector_reorder %nin% blocks])
+  vector_reorder <- append(.hismatch_env$blocks, vector_reorder[vector_reorder %nin% .hismatch_env$blocks])
   dataset[,..vector_reorder]
 }
 
 #' Run all
 #' @export
-run <- function(data1, data2, firstname=NULL, surname=NULL, 
-                firstname1=NULL, surname1=NULL, 
-                firstname2=NULL, surname2=NULL,
-                dist_thr, rel_thr, blocks, workers_input=1){
+runMatching <- function(){
   
-  future::plan(multisession, workers = workers_input)
+  plan(multisession, workers = .hismatch_env$workers)
 
-  if (is.not.null(firstname) & is.not.null(surname)) {
-    firstname1 <- firstname;firstname2 <- firstname
-    surname1 <- surname;surname2 <-surname
-  }
-
-  tmp1 <- hismatch:::setupData(data=data1,
-                     firstname=paste0(firstname1), 
-                     surname=paste0(surname1), 
-                     block_vector = blocks, 
-                     suffix='_1')
+  tmp1 <- hismatch:::setupData(data_input=.hismatch_env$data1, suffix='_1')  
+  tmp2 <- hismatch:::setupData(data_input=.hismatch_env$data2, suffix='_2')
   
-  
-  tmp2 <- hismatch:::setupData(data=data2,
-                     firstname=paste0(firstname2), 
-                     surname=paste0(surname2), 
-                     block_vector = blocks, 
-                     suffix='_2')
-  
-  
-  blocks_tmp <- merge(tmp1[[3]], tmp2[[3]], by=c(blocks))
+  blocks_tmp <- merge(tmp1[[3]], tmp2[[3]], by=c(.hismatch_env$blocks))
   
   merged_data <- with_progress({
-    hismatch:::executeLinking(data1=tmp1[[2]], 
-                   data2=tmp2[[2]],
-                   blocks_data=blocks_tmp, 
-                   dist_thr=dist_thr, 
-                   rel_thr=rel_thr, 
-                   blocks=blocks)
+    hismatch:::executeLinking(data1=tmp1[[2]], data2=tmp2[[2]], blocks_data=blocks_tmp)
   })
 
-  if(rel_thr!=FALSE){merged_data <- merged_data[rel1<rel_thr | is.na(rel1)][rel2<rel_thr | is.na(rel2)]}
-  if(dist_thr!=FALSE){merged_data <- merged_data[dist>dist_thr]}
+  if(.hismatch_env$rel_thr!=FALSE){
+      merged_data <- merged_data[rel1<.hismatch_env$rel_thr | is.na(rel1)][
+      rel2<.hismatch_env$rel_thr | is.na(.hismatch_env$rel2)]}
+  if(.hismatch_env$dist_thr!=FALSE){
+      merged_data <- merged_data[dist>.hismatch_env$dist_thr]}
   
-  merged_data <- hismatch:::mergeBackInData(merged_data, tmp1[[1]], tmp2[[1]][,c(blocks):=NULL])
-  merged_data <- hismatch:::vectorReorder(dataset=merged_data, blocks=blocks)
+  merged_data <- hismatch:::mergeBackInData(merged_data, tmp1[[1]], tmp2[[1]][,c(.hismatch_env$blocks):=NULL])
+  merged_data <- hismatch:::vectorReorder(dataset=merged_data)
 
   return(list(merged_data, tmp1[[1]], tmp2[[1]]))
 }
+
+
+
 
 #' Produce matching statistics
 #' @export
