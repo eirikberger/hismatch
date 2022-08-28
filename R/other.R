fb4bcc7a146b122f00f722791d0ec6dedcc7bd41
Eirik Berger <eirik.berger@gmail.com>
2022-08-28 12:11:39 +0200

    adding savings

--- R/other.R
+++ R/other.R
@@ -3,3 +3,14 @@
 
 #' @export
 `is.not.null` <- Negate(`is.null`)
+
+#' @export
+saveHismatch = function(class_name, filepath){
+  code_string <- paste0('saveRDS(', class_name, ', "', filepath, '")')
+  eval(parse(text=code_string))
+}
+
+#' @export
+readHismatchClass = function(filepath){
+  readRDS(here::here(filepath))
+}
